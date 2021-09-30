68ce08a654a4d75aaa18e936ae45a27bbc5d1157
Jbumk <91252339+Jbumk@users.noreply.github.com>
2021-09-30 23:02:07 +0900

    21-09-30
    stage 2-1프리팹추가
    손전등기능 추가

---
+++ Assets/Scripts/Con_Player/HandLight.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandLight : MonoBehaviour
+{
+    public Light handligh;
+    public GameObject FirCam;
+    public GameObject ThrCam;
+    private bool isOn = false;
+
+    private double CoolTIme = 0.5;
+    private float Timer = 0;
+    private Quaternion LightVec;
+
+
+
+    private void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Timer >= CoolTIme)
+        {
+            if (Input.GetKey(KeyCode.G))
+            {
+                if (isOn)
+                {
+                    handligh.intensity = 0;
+                    isOn = false;
+                    Timer = 0;
+                }
+                else
+                {
+                    handligh.intensity = 15;
+                    isOn = true;
+                    Timer = 0;
+                }
+            }
+        }
+
+        if (Con_Camera.FirCamOn)
+        {
+            LightVec =FirCam.transform.rotation;
+            LightVec.y = 0;
+            LightVec.z = 0;
+            transform.localRotation = LightVec;
+        }
+        else
+        {
+            LightVec = ThrCam.transform.rotation;
+            LightVec.y = 0;
+            LightVec.z = 0;
+            transform.localRotation = LightVec;
+        }
+    }
+}
