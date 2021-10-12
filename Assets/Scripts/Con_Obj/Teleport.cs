7f092f9e7c60c1748cd60817057738db0afc289b
Jbumk <91252339+Jbumk@users.noreply.github.com>
2021-10-13 01:52:28 +0900

    21-10-13
    Teleport추가
    stage2-7난이도 변경
    stage3-1추가

---
+++ Assets/Scripts/Con_Obj/Teleport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject NextPoint;
+   
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Consum_System.max_Pos = NextPoint.transform.position.y;
+            col.transform.position = NextPoint.transform.position+ NextPoint.transform.forward*2f;
+        }
+    }
+}
