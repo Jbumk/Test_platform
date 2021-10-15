ff498a9d8412a7747c966f4cb3c2cabd422757bf
Jbumk <91252339+Jbumk@users.noreply.github.com>
2021-10-15 13:03:12 +0900

    21-10-15
    stage 3-2추가

--- Assets/Scripts/Con_Obj/Teleport.cs
+++ Assets/Scripts/Con_Obj/Teleport.cs
@@ -14,16 +14,19 @@ public class Teleport : MonoBehaviour
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            rigid = col.GetComponent<Rigidbody>();
-           
-            Debug.Log("물리량" + rigid.velocity);
-            Debug.Log(rigid.velocity.normalized);
-            Power = Mathf.Abs(rigid.velocity.x) +Mathf.Abs(rigid.velocity.y) + Mathf.Abs(rigid.velocity.z);
-           
-            Consum_System.max_Pos = NextPoint.transform.position.y;
-            rigid.velocity = Vector3.zero;
-            col.transform.position = NextPoint.transform.position + NextPoint.transform.forward * 2f;
-            rigid.AddForce(NextPoint.transform.forward * Power, ForceMode.Impulse);
+            if (NextPoint != null)
+            {
+                rigid = col.GetComponent<Rigidbody>();
+
+                Debug.Log("물리량" + rigid.velocity);
+                Debug.Log(rigid.velocity.normalized);
+                Power = Mathf.Abs(rigid.velocity.x) + Mathf.Abs(rigid.velocity.y) + Mathf.Abs(rigid.velocity.z);
+
+                Consum_System.max_Pos = NextPoint.transform.position.y;
+                rigid.velocity = Vector3.zero;
+                col.transform.position = NextPoint.transform.position + NextPoint.transform.forward * 2f;
+                rigid.AddForce(NextPoint.transform.forward * Power, ForceMode.Impulse);
+            }
 
             
         }
