8df5ef8bb9e3997e7189382885a6ad56a47a0ab1
Jbumk <91252339+Jbumk@users.noreply.github.com>
2021-12-15 20:36:47 +0900

    21-12-15
    SildeDoor 앞에 Player나 추적자 있을때 열리는 기능 추가
    Stage4-1 Lab추가

--- Assets/Scripts/Con_Obj/Door/Con_Door.cs
+++ Assets/Scripts/Con_Obj/Door/Con_Door.cs
@@ -7,47 +7,108 @@ public class Con_Door : MonoBehaviour
     public GameObject Btn;   
     public GameObject BlockPoint;
     public GameObject OpenPoint;
+    public GameObject Door;
+    public AudioSource DoorSound;
+   
+
     private Renderer Btn_Color;  
     private Button BtnValue;
-    private AudioSource DoorSound;
+   
 
     private bool StateOpen = false;
+
+    public bool Senser = false;
+    //근처에가면 열릴지 확인
+    private bool exist = false;
+    //센서공간 안에 플레이어or 몬스터 존재하는지 확인
   
 
     private void Start()
     {
-        
-        Btn_Color = Btn.GetComponent<Renderer>();
-        BtnValue = Btn.GetComponent<Button>();
-        DoorSound = GetComponent<AudioSource>();
+        if (Btn != null)
+        {
+            Btn_Color = Btn.GetComponent<Renderer>();
+            BtnValue = Btn.GetComponent<Button>();
+        }       
      
     }
    
     private void Update()
     {
-       
-        if (Btn_Color.material.color == Color.green)
+        if (!Senser)
         {
-            if (!StateOpen)
+            if (Btn_Color.material.color == Color.green)
             {
-                DoorSound.Stop();
-                DoorSound.Play();
-                StateOpen = true;
+                if (!StateOpen)
+                {
+                    DoorSound.Stop();
+                    DoorSound.Play();
+                    StateOpen = true;
+                }
+
+                Door.transform.position = Vector3.MoveTowards(Door.transform.position, OpenPoint.transform.position, 3f * Time.deltaTime);
             }
-          
-            transform.position = Vector3.MoveTowards(transform.position, OpenPoint.transform.position, 3f*Time.deltaTime);
+            else
+            {
+                if (StateOpen)
+                {
+                    DoorSound.Stop();
+                    DoorSound.Play();
+                    StateOpen = false;
+                }
+
+                Door.transform.position = Vector3.MoveTowards(Door.transform.position, BlockPoint.transform.position, 3f * Time.deltaTime);
+            }
         }
         else
         {
-            if (StateOpen)
+            if (exist)
             {
-                DoorSound.Stop();
-                DoorSound.Play();
-                StateOpen = false;
-            }
+                if (!StateOpen)
+                {
+                    DoorSound.Stop();
+                    DoorSound.Play();
+                    StateOpen = true;
+                }
 
-            transform.position = Vector3.MoveTowards(transform.position, BlockPoint.transform.position, 3f*Time.deltaTime);
+                Door.transform.position = Vector3.MoveTowards(Door.transform.position, OpenPoint.transform.position, 3f * Time.deltaTime);
+            }
+            else
+            {
+                if (StateOpen)
+                {
+                    DoorSound.Stop();
+                    DoorSound.Play();
+                    StateOpen = false;
+                }
+
+                Door.transform.position = Vector3.MoveTowards(Door.transform.position, BlockPoint.transform.position, 3f * Time.deltaTime);
+            }
         }
+        
      
+    }
+ 
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (Senser)
+        {
+            if (col.gameObject.CompareTag("Player") || col.gameObject.name=="CrabMon")
+            {
+                exist = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (Senser)
+        {
+            if (col.gameObject.CompareTag("Player") || col.gameObject.name == "CrabMon")
+            {
+                exist = false;
+            }
+        }
     }
 }
